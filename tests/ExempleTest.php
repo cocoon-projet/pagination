a91e0345f17f87d61ba0741f76b26d85ebcfe5f1
cocoon-projet <contact@cocoon-projet.fr>
2025-02-01 17:28:13 +0100

    update projet

---
+++ tests/ExempleTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExempleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function testTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
